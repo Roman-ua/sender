335bd8de8b8f8595d7a9949de18878248e52ad69
roman148 <roman@u-coders.team>
2024-10-25 20:04:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'roma@incorporatenow.com',
+		pass: 'lolb molr njzf xvig'
+	}
+});
+
+app.post('/send-email', (req, res) => {
+	const { to, subject, text, html } = req.body;
+
+	const mailOptions = {
+		from: 'roma@incorporatenow.com',
+		to: to, // Кому отправляем
+		subject: subject, // Тема письма
+		text: text, // Текст письма
+		html: html // HTML версия письма
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.error('Ошибка при отправке письма:', error, info);
+			return res.status(500).send('Ошибка при отправке письма');
+		}
+		console.log('Письмо успешно отправлено:', info.response);
+		res.send('Письмо успешно отправлено');
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Сервер запущен на порту ${PORT}`);
+});
